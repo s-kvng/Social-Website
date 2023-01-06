77d03783688eefb17503b212f1d61b6442a9176d
s-kvng <105977478+s-kvng@users.noreply.github.com>
2023-01-06 23:02:24 +0000

    Update
    Created the image model

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.conf import settings
+from django.utils.text import slugify
 
 # Create your models here.
+
+
+class Image(models.Model):
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, 
+                             related_name='images_created', 
+                             on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    url = models.URLField(max_length=2000)
+    image = models.ImageField(upload_to='images/%Y/%m/%d')
+    description = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        
+        index = [
+            models.IndexField('-created')
+        ]
+        
+        ordering = ['-created']
+        
+        
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+            super().save(*args, **kwargs)
+        
+    
+    def __str__(self):
+        return self.title
