555890d70ca206464c16a6a899e8daaac4ae7b86
s-kvng <kvngnathan8420@gmail.com>
2023-02-01 19:47:52 +0000

    Update on Pagination-feature
    Done implementing the infinite pagination feature

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -5,6 +5,8 @@ from .forms import ImageCreateForm
 from .models import Image
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
+from django.core.paginator import Paginator, EmptyPage , PageNotAnInteger
+from django.http import HttpResponse
 
 
 # Create your views here.
@@ -65,6 +67,33 @@ def image_like(request):
             pass
 
     return JsonResponse({'status': 'error'})
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+
+    except EmptyPage:
+        if images_only:
+
+            return HttpResponse('')
+
+        images = paginator.page(paginator.num_pages)
+
+    if images_only:
+        return render(request, 'images/image/list_images.html', {'section': 'images', 'images': images})
+
+    return render(request, 'images/image/list.html', {'section': 'images', 'images': images})
+
+
+
 
 
 
