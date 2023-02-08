ac22826426c44d6eab79dfa246c829dd9f984346
s-kvng <kvngnathan8420@gmail.com>
2023-02-08 23:57:51 +0000

    js for Follow-feature

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse , JsonResponse
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm , UserRegistrationForm , UserEditForm , ProfileEditForm
 from django.contrib.auth.decorators import login_required
@@ -7,6 +7,9 @@ from .models import Profile
 from django.contrib import messages
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
+from django.views.decorators.http import require_POST
+from .models import Contact
+
 
 
 # Create your views here.
@@ -106,6 +109,31 @@ def user_detail(request, username):
     user = get_object_or_404(User, username=username,is_active=True)
 
     return render(request, 'account/user/detail.html', {'section':'people', 'user':user})
+
+
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+
+    if user_id and action:
+
+        try:
+            user = User.objects.get(id=user_id)
+
+            if action == 'follow':
+                Contact.objects.get_or_create(user_from = request.user, user_to = user)
+            else:
+                Contact.objects.filter(user_from = request.user, user_to= user).delete
+
+
+            return JsonResponse({'status' : 'ok'})
+
+        except:
+            return JsonResponse({"status": "error"})
+
+    return JsonResponse({'status':'ok'})
 
 
 
